272c44b1a5eedcd96c14839a9413f58d54ece9bb
cheungbh <534660436@qq.com>
2022-01-11 13:11:27 +0800

    modify customi

--- utils/datasets.py
+++ utils/datasets.py
@@ -371,8 +371,8 @@ class LoadStreams:
 
 def img2label_paths(img_paths):
     # Define label paths as a function of image paths
-    sa, sb = os.sep + 'images' + os.sep, os.sep + 'labels' + os.sep  # /images/, /labels/ substrings
-    return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
+    sa, sb = os.sep, os.sep + 'labels' + os.sep  # /images/, /labels/ substrings
+    return [x.replace("coco", "coco/labels").rsplit('.', 1)[0] + '.txt' for x in img_paths]
 
 
 class LoadImagesAndLabels(Dataset):
